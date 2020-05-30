d617b20b27ea3d27a9c5b67171f7f07de850c60f
Randheer Ramesh K <randheerrrk@gmail.com>
2020-05-30 12:56:23 +0530

    Updates

---
+++ precache-manifest.a9eb5ade372eb095c99dd7ddf96cf5b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73b77599a37ef1e8361582357c27b3c6",
+    "url": "/neon.github.io/index.html"
+  },
+  {
+    "revision": "3da0150dc44c68246028",
+    "url": "/neon.github.io/static/css/main.f0a888fb.chunk.css"
+  },
+  {
+    "revision": "153f1cd7cc32f427cda1",
+    "url": "/neon.github.io/static/js/2.57787c0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/neon.github.io/static/js/2.57787c0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3da0150dc44c68246028",
+    "url": "/neon.github.io/static/js/main.9bbd1b9c.chunk.js"
+  },
+  {
+    "revision": "13af8d4f42d70cbe1589",
+    "url": "/neon.github.io/static/js/runtime-main.99979976.js"
+  }
+]);
