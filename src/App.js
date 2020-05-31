f4dac340c7a13113053d59461e7c75ba4a7402ff
randheerrrk <randheerrrk@gmail.com>
2020-05-31 18:18:19 +0530

    added changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1 class='light' data-text="Life is Good">Life is Good</h1>
+      <div class="light ok" data-text="This is Absolute">This is Absolute</div>
     </div>
   );
 }
